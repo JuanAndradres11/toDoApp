77a63a39384379cd250f8559d826afda7f49f2b2
Juan Andrades <11juanandrades@gmail.com>
2025-04-24 19:53:26 +0530

    Initial commit

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
 DB_USER = os.getenv("DB_USER", "admin")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "")
-DB_HOST = os.getenv("DB_HOST", "")
-DB_NAME = os.getenv("DB_NAME", "database-1")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "juan9811and")
+DB_HOST = os.getenv("DB_HOST", "mydb.crum4qey00uo.us-east-1.rds.amazonaws.com")
+DB_NAME = os.getenv("DB_NAME", "mydb")
 
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+#DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+DATABASE_URL = "mysql+pymysql://admin:juan9811and@mydb.crum4qey00uo.us-east-1.rds.amazonaws.com/mydb"
